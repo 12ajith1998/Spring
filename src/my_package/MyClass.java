6227019ca5d5a608f1242eff4739ea2cf0b13e72
AJITH KUMAR A <12ajith1998@gmail.com>
2021-02-23 18:47:17 +0530

    Add files via upload

---
+++ src/my_package/MyClass.java
@@ -0,0 +1,17 @@
+package my_package;
+
+public class MyClass {
+	
+    public static void main(String[] args) 
+    { 
+    
+    	int i=10;
+    	int j=20;
+    	while(i++<--j)
+    	{
+    		
+    	}
+    	System.out.println(i+" "+j);
+    	
+    }
+}
